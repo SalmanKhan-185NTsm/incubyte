7d2fca2bbfbc29abac17a325123b01d6724157ca
SalmanKhan-185NTsm <salmankhans185@gmail.com>
2025-02-03 16:36:17 +0530

    added jest

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,62 @@
 import { useRef, useState } from "react";
 import "./App.css";
+export function extractDelimiter(input) {
+  const regex = /^\/\/(.+)\n/;
+  const match = input.match(regex);
+  if (match) {
+    return { pattern: match[0], delimiter: match[1] };
+  }
+  return null;
+}
+
+export function splitNumbers(numbers, delimiters) {
+  const delimiterRegex = new RegExp(`[${delimiters.join("")}]`);
+  return numbers.split(delimiterRegex).map((str) => parseInt(str));
+}
+
+export function removeCustomDelimiter(pattern, stringValue) {
+  const temp = stringValue.replace(pattern, "");
+  return temp;
+}
+
+export function cleanInput(input) {
+  // Replace escaped newline characters with actual newline characters
+  return input.replace(/\\n/g, "\n");
+}
+
+export function addNumbers(numbers) {
+  //empty string
+  if (numbers.length === 0 || numbers.trim() === "") {
+    return 0;
+  }
+
+  //list of delimeters
+  const supportedDelimiters = [",", "\n"];
+  // debugger;
+
+  numbers = cleanInput(numbers);
+  // debugger;
+  //check if there is custorm delimeter
+  const customDelimiter = extractDelimiter(numbers);
+
+  let stringValue = numbers;
+  if (customDelimiter !== null) {
+    supportedDelimiters.push(customDelimiter["delimiter"]);
+    stringValue = removeCustomDelimiter(
+      customDelimiter["pattern"],
+      stringValue
+    );
+  }
+  // debugger;
+  const list = splitNumbers(stringValue, supportedDelimiters);
+  const totalNumbers = list.length;
+  let sum = 0;
+  for (let i = 0; i < totalNumbers; i++) {
+    sum += Number(list[i]);
+  }
+  console.log(sum);
+  return sum;
+}
 
 function App() {
   const inputRef = useRef();
@@ -8,32 +65,14 @@ function App() {
     const inputValue = inputRef.current.value;
     const result = addNumbers(inputValue);
     console.log("sum is", result);
-    alert(result.sum);
+    alert(result);
   };
-
 
   //tests
   // addNumbers("1\n2,3,4,5,6\n5");
+  // addNumbers("//;\n1\n2;3;4;5;6\n5");
+  // addNumbers("//;\n1;2;3");
 
-  function splitNumbers(numbers, delimiters) {
-    const delimiterRegex = new RegExp(`[${delimiters.join("")}]`);
-    return numbers.split(delimiterRegex).map((str) => parseInt(str));
-  }
-  function addNumbers(value) {
-    //empty string
-    if (value.length === 0 || value.trim() === "") {
-      return 0;
-    }
-    //get delimeter
-    const list = splitNumbers(value, [",", "\n"]);
-    debugger;
-    const totalNumbers = list.length;
-    let sum = 0;
-    for (let i = 0; i < totalNumbers; i++) {
-      sum += Number(list[i]);
-    }
-    return sum;
-  }
   return (
     <>
       <div className="card">
