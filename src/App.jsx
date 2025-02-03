8ead639bb1e84cf706b22f5e59b57bb670a2f2f1
SalmanKhan-185NTsm <salmankhans185@gmail.com>
2025-02-03 15:05:09 +0530

    Added sum function

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,36 @@ import "./App.css";
 
 function App() {
   const inputRef = useRef();
-  const handleSubmit = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
     const inputValue = inputRef.current.value;
+    const result = addNumbers(inputValue);
+    console.log("sum is",result);
   };
+
+  function addNumbers(value) {
+    const list = value.split(",");
+    const totalNumbers = list.length;
+    let sum = 0;
+    for (let i = 0; i < totalNumbers; i++) {
+      sum += list[i];
+    }
+    return sum;
+  }
   return (
     <>
-      <h2>String Calculator</h2>
-      <form className="form-container" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          className="form-control"
-          name="string_numbers"
-          ref={inputRef}
-        />
-        <button className="btn"> Calculate </button>
-      </form>
+      <div className="card">
+        <h2>String Calculator</h2>
+        <form className="form-container" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            className="form-control"
+            name="string_numbers"
+            ref={inputRef}
+          />
+          <button className="btn"> Calculate </button>
+        </form>
+      </div>
     </>
   );
 }
